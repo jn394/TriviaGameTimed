d87041c4cd4cfcf08478dce0e29930e36db1d5cb
unknown <jayng168@gmail.com>
2018-09-01 23:46:22 -0400

    Updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,8 @@ $(document).ready(function () {
                 c: "Ghost",
                 d: "Hootie"
             },
-            correct: "Hedwig"
+            correct: "Hedwig",
+            source: "https://media.giphy.com/media/hEnME7syCxHrO/giphy.gif"
         },
         {
             question: "Emma Watson is known for playing which character in Harry Potter?",
@@ -20,7 +21,8 @@ $(document).ready(function () {
                 c: "Ginny Weasley",
                 d: "Cho Chang"
             },
-            correct: "Hermione Granger"
+            correct: "Hermione Granger",
+            source: "https://assets.rbl.ms/10639477/980x.gif"
         },
         {
             question: "What is Harry Potter's House in Hogwarts?",
@@ -30,7 +32,8 @@ $(document).ready(function () {
                 c: "Slytherin",
                 d: "Gryffindor"
             },
-            correct: "Gryffindor"
+            correct: "Gryffindor",
+            source: "https://media1.tenor.com/images/3a943d0a82fac73aabb9e1bc57b24800/tenor.gif?itemid=5903488"
         },
         {
             question: "In what year was the first Harry Potter movie released?",
@@ -40,7 +43,8 @@ $(document).ready(function () {
                 c: "2001",
                 d: "2002"
             },
-            correct: "2001"
+            correct: "2001",
+            source: "http://1.bp.blogspot.com/-LypJblUyzZ0/VSAp9dLeMdI/AAAAAAAAGs8/yMccdXJL0CI/s1600/tumblr_lubwx9inBS1qhjhoao1_500.gif"
         },
         {
             question: "Among the wizarding community, the term Muggle refers to what kind of person?",
@@ -50,7 +54,8 @@ $(document).ready(function () {
                 c: "A non-magical person from a non-magical family",
                 d: "A non-magical person from a magical family"
             },
-            correct: "A non-magical person from a non-magical family"
+            correct: "A non-magical person from a non-magical family",
+            source: "https://media.giphy.com/media/MRNsTRaOIZJMk/giphy.gif"
         },
         {
             question: "What colour is the Hogwarts Express?",
@@ -60,7 +65,8 @@ $(document).ready(function () {
                 c: "Indigo",
                 d: "Green"
             },
-            correct: "Scarlet"
+            correct: "Scarlet",
+            source: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/1441130072-tumblr-mfr8ykndgp1qmt5mvo1-500-1495311052.gif?crop=1xw:1xh;center,top&resize=480:*"
         },
         {
             question: "How are letters sent in the wizarding world?",
@@ -70,7 +76,8 @@ $(document).ready(function () {
                 c: "Via postmen",
                 d: "Via owls"
             },
-            correct: "Via owls"
+            correct: "Via owls",
+            source: "https://78.media.tumblr.com/22717e68b1d96864e289158317ec817c/tumblr_p2amy1Ewkj1uokcj7o1_500.gif"
         },
         {
             question: "Who played Lord Voldemort in the movies?",
@@ -80,7 +87,8 @@ $(document).ready(function () {
                 c: "Gary Oldman",
                 d: "Ralph Fiennes"
             },
-            correct: "Ralph Fiennes"
+            correct: "Ralph Fiennes",
+            source: "http://24.media.tumblr.com/99a4f2e42870617637aa87924544cb96/tumblr_mla4975vsN1rd5otro1_500.gif"
         },
         {
             question: "What is the golden ball in Quidditch called?",
@@ -90,7 +98,8 @@ $(document).ready(function () {
                 c: "Bludger",
                 d: "Wiffles"
             },
-            correct: "Snitch"
+            correct: "Snitch",
+            source: "https://media.giphy.com/media/IlVul9hwHHy9O/giphy.gif"
         },
         {
             question: "From what King's Cross platform does the Hogwarts Express leave?",
@@ -100,14 +109,13 @@ $(document).ready(function () {
                 c: "Five & A Half",
                 d: "Eleventeen"
             },
-            correct: "Nine & Three-Quarters"
+            correct: "Nine & Three-Quarters",
+            source: "https://img.buzzfeed.com/buzzfeed-static/static/2015-09/1/5/enhanced/webdr14/anigif_enhanced-21684-1441100277-2.gif?downsize=715:*&output-format=auto&output-quality=auto"
         }
     ];
 
-    //Hiding display and submit buttons
+    //Hiding display
     $("#display").hide();
-    $("#submitButton").hide();
-
 
     //Variable timer will hold the setInterval when we start the slideshow
     var intervalId;
@@ -125,13 +133,15 @@ $(document).ready(function () {
     //Two mintue count down.
     var countDown = {
 
-        time: 5,
+        time: 30,
 
         //A function that at every second it applys the timeCount function.
         startTimer: function () {
-            countDown.time = 5;
+            countDown.time = 30;
             intervalId = setInterval(countDown.timeCount, 1000);
             countDown.displayQuestions();
+            
+            
         },
 
         //A function that minuses 1 secound 
@@ -139,26 +149,22 @@ $(document).ready(function () {
             countDown.time--;
             $("#display").text(countDown.timeConverter(countDown.time));
 
-
-
+            //If timer is 0 the answer is revealed
             if (countDown.time <= 0) {
-                countDown.time = 5;
-                countDown.displayQuestions();
+                countDown.time = 30;
+                setTimeout(countDown.showResutlsWrong(), 100000);
             }
-            //When the time runs out it should swtich to the next question.
-            // if (countDown.time <= 0) {
-            //     clearInterval(intervalId);
-            //     $("#display").hide();
-            //     $("#questions").hide();
-            //     countDown.myResults();
-            // };
+
         },
+
+        //showing the question
         displayQuestions: function () {
-            //showing the question
+            
+            //Keeps track of which question the loop is on
             count++;
             setTimeout(countDown.nextQuestion, 1000);
 
-            // TODO: If the count is the same as the length of the image array, reset the count to 0.
+            //If the count is the same as the length of the image array game ends
             if (count === myQuestions.length) {
                 clearInterval(intervalId);
                 $("#display").hide();
@@ -166,12 +172,16 @@ $(document).ready(function () {
                 countDown.myResults();
             }
         },
+
+        //Shows the next question
         nextQuestion: function () {
             $("#questions").empty();
+            $("#gifs").empty();
+            $("#results").empty();
             $("#questions").append(myQuestions[count].question);
             correctAnswers.push(myQuestions[count].correct.replace(/\s/g, ''));
             var innerLoop = myQuestions[count].answers;
-console.log(correctAnswers);
+            console.log(correctAnswers);
 
             //This for loop starts a loop in the "answers" object 
             for (var letter in innerLoop) {
@@ -181,34 +191,37 @@ console.log(correctAnswers);
 
             };
 
+            //When a div with the class .ans is clicked it will trigger an event
             $(".ans").on("click", function () {
                 console.log($(this).text());
                 var clickedAnswer = $(this).text().replace(/\s/g, '');
                 if (correctAnswers.includes(clickedAnswer)) {
-                    console.log("Correct!");
-                    setTimeout(countDown.showResutls(), 1000);
+                    setTimeout(countDown.showResutlsRight(), 100000);
                     numberCorrect++;
                 }
                 else {
-                    console.log("Wrong!");
+                    setTimeout(countDown.showResutlsWrong(), 100000);
                 };
             })
-            //set values to clickable answers
-            //if clicked value is equal to correct answer show results otherwise show results
-
         },
 
-        showResutls: function () {
+        //A function that will display something when its correct
+        showResutlsRight: function () {
             clearInterval(intervalId);
+            $("#results").append("<h3>" + "Correct!!!" + "</h3>");
             $("#questions").empty();
-            $("#results").append("<h1>" + "Correct!!!" + "</h1>");
-            $("#gifs").attr("src", "https://i.imgur.com/gC7ejhu.gif");
+            $("#gifs").append("<img src=" + myQuestions[count].source + ">");
             countDown.startTimer();
         },
 
-
-
-
+        //A function that will display something when its wrong
+        showResutlsWrong: function () {
+            clearInterval(intervalId);
+            $("#results").append("<h3>" + "Wrong!!!" + " The correct answer was: " + myQuestions[count].correct + "</h3>");
+            $("#questions").empty();
+            $("#gifs").append("<img src=" + myQuestions[count].source + ">");
+            countDown.startTimer();
+        },
 
         //Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
         timeConverter: function (t) {
@@ -228,63 +241,31 @@ console.log(correctAnswers);
             return minutes + ":" + seconds;
         },
 
-        //A function that shows the questions and answers
-        showQuestions: function () {
-            for (var i = 0; i < myQuestions.length; i++) {
-                //Adds the questions into the "#questions" div
-                correctAnswers.push(myQuestions[i].correct.replace(/\s/g, ''));
-
-                var innerLoop = myQuestions[i].answers;
-
-                //This for loop starts a loop in the "answers" object 
-                for (var letter in innerLoop) {
-                    //Adds the answers into the "#questions" div
-                    console.log(innerLoop[letter]);
-                    $("#questions").append("<br>" + "<input type='radio' class='form-check-input ans' name=" + i + " value=" + innerLoop[letter].replace(/\s/g, '') + " >" + "<label class='form-check-label' for='exampleRadios1'>" + innerLoop[letter] + "</label>");
-                };
-            };
-        },
-
-        //A functions that when button is clicked the game ends and score is releaved.
-        mySubmit: function () {
-            $("#submitButton").on("click", function () {
-                clearInterval(intervalId);
-                $("#display").hide();
-                $("#questions").hide();
-                $("#submitButton").hide();
-                countDown.myResults();
-            });
-        },
-
         //if startment showing percentage of right answers
         myResults: function () {
-            //For loop that goes through all radio buttons that were checked and sees if the values of the radio button are in the correctAnswers array
-            for (var j = 0; j < myQuestions.length; j++) {
-                console.log($("input[name=" + j + "]:checked").val());
-                if (correctAnswers.includes($("input[name=" + j + "]:checked").val())) {
-                    numberCorrect++;
-                }
-            };
+
             $("#results").show();
+            $("#gifs").empty();
+
             score = (numberCorrect / numberQuestions) * 100;
             if (score <= 30) {
-                $("#gifs").attr("src", "https://media.giphy.com/media/XgIvYDNkLh49q/giphy.gif");
+                $("#gifs").append("<img src='https://media.giphy.com/media/XgIvYDNkLh49q/giphy.gif'>");
                 $("#results").append("<h3>" + "WOW You Really Are a Muggle" + "</h3>");
             }
             else if (score > 30 && score <= 50) {
-                $("#gifs").attr("src", "http://mrwgifs.com/wp-content/uploads/2013/04/Daniel-Radcliffe-Crying-Gif-In-Harry-Potter.gif");
+                $("#gifs").append("<img src='http://mrwgifs.com/wp-content/uploads/2013/04/Daniel-Radcliffe-Crying-Gif-In-Harry-Potter.gif'>");
                 $("#results").append("<h3>" + "Awww It's Ok Atleast You Tried" + "</h3>");
             }
             else if (score > 50 && score <= 70) {
-                $("#gifs").attr("src", "http://lovelace-media.imgix.net/uploads/62/5889f760-a701-0131-b6c9-4ab1d83132ba.gif?");
+                $("#gifs").append("<img src='http://lovelace-media.imgix.net/uploads/62/5889f760-a701-0131-b6c9-4ab1d83132ba.gif?'>");
                 $("#results").append("<h3>" + "Good Job! You're Pretty Good" + "</h3>");
             }
             else if (score > 70 && score <= 90) {
-                $("#gifs").attr("src", "https://media1.tenor.com/images/e8499a0f268be7bc76f80dd453ad2fe2/tenor.gif?itemid=4794040");
+                $("#gifs").append("<img src='https://media1.tenor.com/images/e8499a0f268be7bc76f80dd453ad2fe2/tenor.gif?itemid=4794040'>");
                 $("#results").append("<h3>" + "Amazing! You're Really Good" + "</h3>");
             }
             else if (score = 100) {
-                $("#gifs").attr("src", "http://cdn1.alloy.com/wp-content/uploads/2015/06/harry-potter-wow.gif");
+                $("#gifs").append("<img src='http://cdn1.alloy.com/wp-content/uploads/2015/06/harry-potter-wow.gif'>");
                 $("#results").append("<h3>" + "WOW!!! Somebody Read Their Books! A True Potter Head!" + "</h3>");
             };
             $("#score").append("<h3>" + "You Got " + score + "%" + " Correct!" + "</h3>");
@@ -296,11 +277,8 @@ console.log(correctAnswers);
         //show all the question
         $("#startButton").hide();
         $("#display").show();
-        $("#submitButton").show();
         countDown.startTimer();
-        // countDown.showQuestions();
-        countDown.mySubmit();
-        // console.log(correctAnswers);
+
     });
 
 });
